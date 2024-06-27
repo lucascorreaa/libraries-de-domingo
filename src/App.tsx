3b2abc0e98458a13c4a553f69e334b770f4ab246
lucascorreaa <lucas.lca14@gmail.com>
2024-06-26 21:31:08 -0300

    feat: created Pokedex de Domingo repository

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Pokedex de Domingo</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
